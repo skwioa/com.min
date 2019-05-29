bc685ec1a3b22b889e70e6a9bccafd9074fc8022
Da <skwioa@gmail.com>
2019-05-30 04:52:03 +0800

    Initial commit

---
+++ src/main/java/com/min/Person.java
@@ -0,0 +1,7 @@
+package com.min;
+
+public class Person {
+    public static void hello() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/kotlin/com/min/Hello.kt
@@ -0,0 +1,13 @@
+package com.min
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+ var n =Human()
+    n.hello()
+}
+class Human { fun hello(){
+    println("Hello kotlin")
+}
+
+}
